31aa9d19df45db64f0c90e4b54e722a424cbf838
Kd526 <davisthoyah@gmail.com>
2025-05-04 00:29:46 +0300

    add ERC20 uups

--- contracts/NativeToken.sol
+++ contracts/NativeToken.sol
@@ -4,23 +4,56 @@ pragma solidity ^0.8.23;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract UpgradeableToken1 is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20PausableUpgradeable, OwnableUpgradeable {
+contract ChainZNative is
+    Initializable,
+    UUPSUpgradeable,
+    OwnableUpgradeable,
+    ReentrancyGuardUpgradeable,
+    ERC20Upgradeable,
+    ERC20BurnableUpgradeable,
+    ERC20PausableUpgradeable
+{
+    address private mtzAdmin;
+    address public dao;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(address initialOwner) initializer public {
-        __ERC20_init("UpgradeableToken", "UTK");
+    // Modifier that allows only the admin to call a function
+    modifier onlyAdmin() {
+        require(
+            msg.sender == mtzAdmin,
+            "Unauthorized: caller is not the admin"
+        );
+        _;
+    }
+    modifier onlyDao() {
+        require(msg.sender == dao, "Unauthorized: caller is not the dao admin");
+        _;
+    }
+
+    function initialize(address _dao) public initializer {
+        UUPSUpgradeable.__UUPSUpgradeable_init();
+        OwnableUpgradeable.__Ownable_init(_dao);
+        ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
+        __ERC20_init("CHAINZ", "CHZ");
         __ERC20Burnable_init();
         __ERC20Pausable_init();
-        __Ownable_init(initialOwner);
 
-        _mint(msg.sender, 10000 * 10 ** decimals());
+        _mint(msg.sender, 10_000_000_000 * 10 ** decimals());
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal virtual override onlyOwner {}
 
     function pause() public onlyOwner {
         _pause();
@@ -30,16 +63,22 @@ contract UpgradeableToken1 is Initializable, ERC20Upgradeable, ERC20BurnableUpgr
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyDao nonReentrant() {
         _mint(to, amount);
     }
 
+      function burn(address account, uint256 amount) public onlyDao nonReentrant() {
+        _burn(account, amount);
+    }
+
+
     // The following functions are overrides required by Solidity.
 
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20Upgradeable, ERC20PausableUpgradeable)
-    {
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20Upgradeable, ERC20PausableUpgradeable) {
         super._update(from, to, value);
     }
 }
